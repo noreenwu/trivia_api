a26349066bb2ad36ca80e98e90281444ae9b1c89
Noreen Wu <noreenwu@gmail.com>
2019-12-04 11:14:39 -0500

    style: tweaked font sizes and cursor settings

--- starter/frontend/src/components/Question.js
+++ starter/frontend/src/components/Question.js
@@ -17,19 +17,19 @@ class Question extends Component {
     const { question, answer, category, difficulty } = this.props;
     return (
       <div className="Question-holder">
-        <div className="Question">{question}</div>
         <div className="Question-status">
           <img className="category" alt="" src={`${category}.svg`}/>
           <div className="difficulty">Difficulty: {difficulty}</div>
-          <img src="delete.png" alt="delete question" className="delete" onClick={() => this.props.questionAction('DELETE')}/>
-          
+          <img src="delete.png" alt="delete question" className="delete" onClick={() => this.props.questionAction('DELETE')}/>          
         </div>
+        <div className="Question">{question}</div>        
         <div className="show-answer button"
             onClick={() => this.flipVisibility()}>
             {this.state.visibleAnswer ? 'Hide' : 'Show'} Answer
           </div>
         <div className="answer-holder">
-          <span style={{"visibility": this.state.visibleAnswer ? 'visible' : 'hidden'}}>Answer: {answer}</span>
+          <span style={{"visibility": this.state.visibleAnswer ? 'visible' : 'hidden'}}>
+            <span className="answer-label">Answer: </span>{answer}</span>
         </div>
       </div>
     );
